62d6c800511364bd8b7f3f77bcf527021641d6f2
Thunderman221 <119857501+Thunderman221@users.noreply.github.com>
2024-07-16 11:16:28 +0300

    load task 2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,41 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+document.querySelector('.form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delay = parseInt(event.target.delay.value);
+  const state = event.target.state.value;
+
+  createPromise(delay, state)
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        timeout: 5000,
+        class: 'iziToast-custom-success',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        timeout: 5000,
+        class: 'iziToast-custom-error',
+      });
+    });
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
